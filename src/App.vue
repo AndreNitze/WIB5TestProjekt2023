1b2f6a1f97ed0817d3ececf8f840c57d4374b9a2
muellejo <jm784952@gmail.com>
2022-11-02 15:15:39 +0100

    text message

--- src/App.vue
+++ src/App.vue
@@ -9,7 +9,7 @@ import HelloWorld from './components/HelloWorld.vue'
 
     <div class="wrapper">
      <!-- <HelloWorld msg="Du hast..." />-->
-      Bitte diesen Text hier ändern! Grüße von Marvin
+      Bitte diesen Text hier ändern! Grüße von Johannes
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
