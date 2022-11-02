c584055ece58c301d6bbf7c3513b1aa819870195
JRabov <janarabov@gmail.com>
2022-11-02 15:28:48 +0100

    geändert

--- src/App.vue
+++ src/App.vue
@@ -9,7 +9,8 @@ import HelloWorld from './components/HelloWorld.vue'
 
     <div class="wrapper">
      <!-- <HelloWorld msg="Du hast..." />-->
-      Version 3.0 Bitte diesen Text hier ändern! Grüße von Marvin Prigenitz
+      Version 4.0
+  
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
